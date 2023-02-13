2598b6d2e91c98be8e3788360fb6bd4cf55f62e1
parsajiravand <parsajiravand@gmail.com>
2023-02-13 16:16:38 +0330

    fix code box theme on light mode

--- .vitepress/theme/components/CodeBox.vue
+++ .vitepress/theme/components/CodeBox.vue
@@ -9,18 +9,18 @@
     </div>
     <div class="col-span-1">
       <section
-        class="flex justify-between flex-row p-2 text-black bg-gray-700 rounded-t-lg"
+        class="flex justify-between flex-row p-2 text-black dark:bg-gray-700 bg-gray-200 rounded-t-lg"
       >
-        <div class="text-white endpoint sticky top-0 z-1"><span class="method">{{method}}</span>  {{ endpoint }}</div>
+        <div class="text-white endpoint sticky top-0 z-1"><span class="method">{{method}}</span>  <span class="dark:text-gray-200 text-gray-700"> {{ endpoint }}</span></div>
         <div class="relative">
           <button
             v-show="!showDropDown"
             @click="showDropDown = true"
-            class="flex flex-row px-3 py-1 rounded-md bg-gray-800 text-gray-300 border-0"
+            class="flex flex-row px-3 py-1 rounded-md dark:bg-gray-800 text-gray-800 dark:text-gray-300 border-0"
           >
             <span class="mr-1">{{ lang }}</span>
             <svg
-              class="w-5 h-5 mt-1 text-white"
+              class="w-5 h-5 mt-1 dark:text-white text-gray-700"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
@@ -35,11 +35,11 @@
           <button
             v-show="showDropDown"
             @click="showDropDown = false"
-            class="flex flex-row px-3 py-1 rounded-md bg-gray-800 text-gray-300 border-0"
+            class="flex flex-row px-3 py-1 rounded-md dark:bg-gray-800 dark:text-gray-300 text-gray-800 border-0"
           >
             <span class="mr-1">{{ lang }}</span>
             <svg
-              class="w-5 h-5 mt-1 text-white"
+              class="w-5 h-5 mt-1 text-white text-gray-700"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
@@ -55,13 +55,13 @@
           <div
             v-show="showDropDown"
             ref="dropDownItems"
-            class="absolute right-0 py-2 mt-2 rounded-md shadow-xl z-10 bg-gray-800"
+            class="absolute right-0 py-2 mt-2 rounded-md shadow-xl z-10 dark:bg-gray-800 bg-gray-200 text-gray-800"
           >
             <div
               :value="item.name"
               v-for="(item, index) in labraries"
               v-show="item.name.toLowerCase() !== langFromQuery"
-              class="block px-4 py-2 text-sm hover:bg-gray-700 text-gray-300 cursor-pointer"
+              class="block px-4 py-2 text-sm hover:bg-gray-700 dark:text-gray-300 cursor-pointer"
               :key="index"
               @click="toggleTabs(item.name, $event)"
             >
@@ -73,7 +73,7 @@
         </div>
       </section>
       <section
-        class="code-section flex justify-between flex-col p-2 bg-gray-800 text-gray-800 rounded-b-lg max-h-screen"
+        class="code-section flex justify-between flex-col p-2 dark:bg-gray-800 text-gray-800 bg-gray-50 rounded-b-lg max-h-screen"
       >
         <slot name="code" />
       </section>
