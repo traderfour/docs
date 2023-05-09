ce8c14a55e48e0f56f4fc4ea78e8d9ca6f9ebef3
briofy-mammad <m.ranjbar@briofy.net>
2023-05-09 17:13:27 +0330

    added basic settings

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+    docus: {
+      title: 'Trader4',
+      description: 'Trader4 Docs',
+      url: 'http://trader4.net/docs',
+      image: '/social-card-preview.png',
+      socials: {
+        twitter: '@trader4',
+        github: 'trader4/docus',
+      },
+    }
+  })
+  
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,5 @@ export default defineNuxtConfig({
   modules: ["@nuxt/content"],
   content: {
     documentDriven: true,
-  },
+  }
 });
