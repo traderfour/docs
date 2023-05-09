7ce70d4d4be781bd2322ecf08cc573f1bd5caa41
briofy-parsajiravand <p.jiravand@briofy.net>
2023-05-09 10:25:03 +0330

    add @nuxt-themes/docus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  extends: '@nuxt-themes/docus',
   modules: ["@nuxt/content"],
   content: {
     documentDriven: true,
