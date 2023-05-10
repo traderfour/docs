7a9444d0512b105bb016a5724bbe3dc819131c12
briofy-mammad <m.ranjbar@briofy.net>
2023-05-10 12:43:26 +0330

    added all images added duply demand and trend line pages fixed some components added json pretty package

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
     docus: {
-      title: 'Trader4',
+      title: 'Trader4 Documentation',
       description: 'Trader4 Docs',
       url: 'http://trader4.net/docs',
       image: '/social-card-preview.png',
       socials: {
         twitter: '@trader4',
-        github: 'trader4/docus',
+        github: 'traderfour',
       },
     }
   })
